ffc673db7c8035bfd3396e62fa8676f74b4f23fe
root <root@stretch.localdomain>
2019-09-27 20:55:31 +0000

    Configuracion de PostgreSQL

--- sic/settings.py
+++ sic/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'sic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'siscontable',
+        'USER': 'sicuser',
+        'PASSWORD': 'sic123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
