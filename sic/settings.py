b0e82a385fe9e54c15e35fe9138f82d7005a250c
Diego Quinteros <diegoquinteros02.04.97@gmail.com>
2019-11-10 16:48:42 -0600

    Agregando whitenoise para staticfile en modo produccion

--- sic/settings.py
+++ sic/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'sic.urls'
@@ -144,3 +145,4 @@ AUTH_USER_MODEL = 'usuario.Usuario'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
