db81751b3727b36ba1fddd83490f476afe9257d0
oscarlizama <le17004@ues.edu.sv>
2019-11-12 22:35:52 +0000

    Eliminar modulos

--- apps/permiso/urls.py
+++ apps/permiso/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.conf.urls import url
-from apps.permiso.views import resumenPermiso
+from apps.permiso.views import resumenPermiso, eliminarPermiso
 
 urlpatterns = [
     path('', resumenPermiso, name="resumenPermiso"),
+    path('eliminar/<int:idCuenta>', eliminarPermiso, name="eliminarPermiso"),
 ]
