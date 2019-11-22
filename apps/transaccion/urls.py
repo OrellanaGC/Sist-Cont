289e5742a2c5c18b7a075345bec6620f4db51cfe
Gabriel Hidalgo <gabrielalejandro84.gh@gmail.com>
2019-11-21 21:50:45 -0600

    trans v0.999

--- apps/transaccion/urls.py
+++ apps/transaccion/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.conf.urls import url
-from apps.transaccion.views import nuevaPartida, agregarTransaccion, eliminarTransaccion, resumenPartida, cargarXCP
+from apps.transaccion.views import nuevaPartida, agregarTransaccion, eliminarTransaccion, resumenPartida, cargarXCP, cancelarPartidaDoble
 
 urlpatterns = [
     path('', resumenPartida, name="resumenPartida"),
     path('<int:cuentaPadre>', cargarXCP, name="cargarXCP"),
     path('nueva', nuevaPartida, name="nuevaPartida"),
     path('agregar', agregarTransaccion, name='agregarTransaccion'),
-    path('eliminar/<int:idTransaccion>', eliminarTransaccion, name='transaccionElminar')
+    path('eliminar', eliminarTransaccion, name='transaccionElminar'),
+    path('cancelar/<str:idsTransacc>',cancelarPartidaDoble, name="cancelarPar")
 ]
